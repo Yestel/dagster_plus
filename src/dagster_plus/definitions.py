4b60cb80a4d6094a079f93c4a619565868a529e1
quan.dao <quan.dao@definer-inc.com>
2025-11-22 05:25:49 +0700

    Minor

--- src/dagster_plus/definitions.py
+++ src/dagster_plus/definitions.py
@@ -1,19 +1,20 @@
 import dagster as dg
+from dagster_plus import assets, schedules
 from dagster_plus.resources.snowflake_db import SnowflakeDB
-from dagster_plus.assets.crawler import hackernews, todos
-from dagster_plus import schedules
+from dagster_plus.resources.postgres_db import PostgresDB
 
 
 @dg.definitions
 def defs():
     return dg.Definitions.merge(
         dg.Definitions(
-            assets=dg.load_assets_from_modules([hackernews, todos]),
+            assets=dg.load_assets_from_package_module(assets),
         ),
         schedules.defs(),
         dg.Definitions(
             resources={
                 "snowflake": SnowflakeDB.configure_at_launch(),
+                "postgres": PostgresDB.configure_at_launch(),
             }
         )
     )
